45052f4aba828470e24d22c91bc1cb589cece4d7
Grainier Perera <grainier@wso2.com>
2016-11-24 15:58:35 +0530

    Moving extension from Siddhi Core repository

--- component/src/main/java/org/wso2/extension/siddhi/evalscript/js/EvalJavaScript.java
+++ component/src/main/java/org/wso2/extension/siddhi/evalscript/js/EvalJavaScript.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c)  2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-package org.wso2.siddhi.extension.evalscript;
+package org.wso2.extension.siddhi.evalscript.js;
 
 import org.wso2.siddhi.core.exception.ExecutionPlanCreationException;
 import org.wso2.siddhi.core.exception.ExecutionPlanRuntimeException;
