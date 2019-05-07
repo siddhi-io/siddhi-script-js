00825a5b0ae92dd4f4d8a01ba10fbfc9c7478519
ajkarki <ajaya.karki30@gmail.com>
2019-05-07 14:07:59 +0530

    Refactor for wso2-extension to siddhi-io repo move

--- component/src/test/java/io/siddhi/extension/script/js/test/util/SiddhiTestHelper.java
+++ component/src/test/java/io/siddhi/extension/script/js/test/util/SiddhiTestHelper.java
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-package org.wso2.extension.siddhi.script.js.test.util;
+package io.siddhi.extension.script.js.test.util;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
